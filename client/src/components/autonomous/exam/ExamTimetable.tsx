6ca68cc118fe3ff77e8a2af4bf2a1fb2850e5ec2
Girdhari Lal <girdhari.lal@poornima.org>
2026-05-04 11:52:59 +0530

    Exam timetable notices

--- client/src/components/autonomous/exam/ExamTimetable.tsx
+++ client/src/components/autonomous/exam/ExamTimetable.tsx
@@ -103,6 +103,86 @@ const ExamTimetable: React.FC = () => {
               <a className="text-primary" href="https://drive.google.com/file/d/1WUWFZ_b-C7Fz-JJl8qpiW7DdB9vaMmhS/view?usp=drive_link">PDF</a>
             </td>
           </tr>
+          <tr>
+            <td className="border p-2">12</td>
+            <td className="border p-2">B. Tech. II Semester Second Mid Term Examinations Time Table May 2026</td>
+            <td className="border p-2">This is to inform you that the timetable for the B. Tech. II Semester Second Mid Term Examinations has been finalized. Please find the enclosed timetable for your reference. You are advised to go through it carefully and prepare accordingly.</td>
+            <td className="border p-2">
+              <a className="text-primary" href="https://drive.google.com/file/d/1-OQWiNpjY63Vkp_52qMlCiPRInK75IlP/view?usp=drive_link">PDF</a>
+            </td>
+          </tr>
+          <tr>
+            <td className="border p-2">13</td>
+            <td className="border p-2">B. Tech. VIII Semester Second Mid Term Examinations Time Table April 2026</td>
+            <td className="border p-2">This is to inform you that the Second Mid Term Examinations for B.Tech VIII Semester are scheduled to commence from 15/04/2026. Please find the timetable enclosed here for your reference.</td>
+            <td className="border p-2">
+              <a className="text-primary" href="https://drive.google.com/file/d/127JEe5C3xOclp8RwP4HyIsqneESIqiwR/view?usp=drive_link">PDF</a>
+            </td>
+          </tr>
+          <tr>
+            <td className="border p-2">14</td>
+            <td className="border p-2">Time Table of Open Book Test (Component of Third Mid Term Examination) for B.Tech. II Semester - April 2026</td>
+            <td className="border p-2">This is to inform you that the Time Table of  Open Book Test (Component of Third Mid Term Examination) for B.Tech II Semester is finalised. Please find the timetable enclosed here for your reference.</td>
+            <td className="border p-2">
+              <a className="text-primary" href="https://drive.google.com/file/d/1EnQiw4or1s8OFT2Xmj1GI_2GrOLvVfVA/view?usp=drive_link">PDF</a>
+            </td>
+          </tr>
+          <tr>
+            <td className="border p-2">15</td>
+            <td className="border p-2">B. Tech. VIII Semester End Term Practical Examinations Time Table - April 2026 (RTU)</td>
+            <td className="border p-2">This is to inform you that the timetable for the B. Tech. VIII Semester End Term Practical Examinations has been finalized. Please find the enclosed timetable for your reference. You are advised to go through it carefully and prepare accordingly.</td>
+            <td className="border p-2">
+              <a className="text-primary" href="https://drive.google.com/file/d/1hzDi9aySV_0SyR9nuwp34wm6mo7wTruT/view?usp=drive_link">PDF</a>
+            </td>
+          </tr>
+          <tr>
+            <td className="border p-2">16</td>
+            <td className="border p-2">Time Table of End Semester Exam, VIII Sem, B.Tech. 2025-26 (RTU Theory)</td>
+            <td className="border p-2">Time Table of VIII Sem, B.Tech, End Semester Exam 2025-26 (RTU) is enclosed here for your reference. You are advised to prepare accordingly and ensure your presence as per the schedule.</td>
+            <td className="border p-2">
+              <a className="text-primary" href="https://drive.google.com/file/d/1Lb5t3v6dybGronoNDGg-S3tR2Vy96qf2/view?usp=drive_link">PDF</a>
+            </td>
+          </tr>
+          <tr>
+            <td className="border p-2">17</td>
+            <td className="border p-2">Time Table of End Semester Exam, VI Sem, B.Tech. 2025-26 (RTU Theory)</td>
+            <td className="border p-2">Time Table of VI Sem, B.Tech, End Semester Exam 2025-26 (RTU) is enclosed here for your reference. You are advised to prepare accordingly and ensure your presence as per the schedule.</td>
+            <td className="border p-2">
+              <a className="text-primary" href="https://drive.google.com/file/d/1Jzxh2FoxOkZ8GJhy37cQuVr9jOJzQRtw/view?usp=drive_link">PDF</a>
+            </td>
+          </tr>
+          <tr>
+            <td className="border p-2">18</td>
+            <td className="border p-2">B. Tech. II Semester End Semester Practical Examination Time Table may 2026.</td>
+            <td className="border p-2">This is to inform you that the End Semester Practical Examinations for B.Tech II Semester are scheduled to commence from 11 May 2026. Please find the timetable enclosed here for your reference.</td>
+            <td className="border p-2">
+              <a className="text-primary" href="https://drive.google.com/file/d/1NmQhN5_gm5vXtbJUZ0BnKus-o3kw5Zue/view?usp=drive_link">PDF</a>
+            </td>
+          </tr>
+          <tr>
+            <td className="border p-2">19</td>
+            <td className="border p-2">Time Table of End Semester Exam, IV Sem, B.Tech. 2025-26 (RTU Theory)</td>
+            <td className="border p-2">The Time Table for the IV Semester B.Tech End Semester Examination 2025–26 (RTU) is enclosed herewith for your reference. All students are advised to carefully review the schedule and make necessary preparations accordingly. </td>
+            <td className="border p-2">
+              <a className="text-primary" href="https://drive.google.com/file/d/11sZptLQZsGS6m17vCVgPIGTyWacf_uLC/view?usp=drive_link">PDF</a>
+            </td>
+          </tr>
+          <tr>
+            <td className="border p-2">20</td>
+            <td className="border p-2">B. Tech. II Semester End Semester Theory Examinations Time Table 2025-26</td>
+            <td className="border p-2">The Time Table for the II Semester B.Tech End Semester Examination 2025–26 is enclosed herewith for your reference. All students are advised to carefully review the schedule and make necessary preparations accordingly. </td>
+            <td className="border p-2">
+              <a className="text-primary" href="https://drive.google.com/file/d/1cTobsgNLMY4t7sMyQdsDd0WZrF_9oNC6/view?usp=drive_link">PDF</a>
+            </td>
+          </tr>
+          <tr>
+            <td className="border p-2">21</td>
+            <td className="border p-2">IV Semester RTU Practical Examination Time Table May 2025-26</td>
+            <td className="border p-2">The Time Table for the IV Semester B.Tech End Semester Examination 2025–26 (RTU) is enclosed herewith for your reference. All students are advised to carefully review the schedule and make necessary preparations accordingly. </td>
+            <td className="border p-2">
+              <a className="text-primary" href="https://drive.google.com/file/d/1m-uxUpZQbyTo3dp9Q52aeg6NJNN_9nwn/view?usp=drive_link">PDF</a>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
